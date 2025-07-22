4ddad6e404e4e02693ef57a81377b85590295189
Christophe Delage <christ@ophe.net>
2025-07-22 09:28:15 +0200

    rename *2

--- build.zig
+++ build.zig
@@ -4,23 +4,23 @@ pub fn build(b: *std.Build) void {
     const tgt = b.standardTargetOptions(.{});
     const opt = b.standardOptimizeOption(.{});
 
-    const argz2 = b.addModule("argz", .{
+    const argz = b.addModule("argz", .{
         .target = tgt,
         .optimize = opt,
         .root_source_file = b.path("argz.zig"),
     });
 
-    const example2 = b.addExecutable(.{
+    const example = b.addExecutable(.{
         .name = "example",
         .target = tgt,
         .optimize = opt,
         .root_source_file = b.path("example.zig"),
     });
-    example2.root_module.addImport("argz", argz2);
+    example.root_module.addImport("argz", argz);
     // b.installArtifact(example2);
 
     // run the CLI tool
-    const run_cmd = b.addRunArtifact(example2);
+    const run_cmd = b.addRunArtifact(example);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| run_cmd.addArgs(args);
 
